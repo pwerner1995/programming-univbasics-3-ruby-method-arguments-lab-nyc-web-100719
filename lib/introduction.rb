9b0703681af4257696f6db4fe709dc702dd546da
Peter Werner <pcw1995@yahoo.com>
2019-09-02 18:21:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,6 +9,11 @@ def introduction(name)
 end
 introduction("Peter")
 
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+introduction_with_language("Peter","Ruby")
+
 
 
 
